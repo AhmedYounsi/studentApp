dd9f3515a01a04700cc3e738bc3cac5b740c07c2
YounsiAhmed <hammoua07@gmail.com>
2022-07-29 03:36:31 +0100

    dfgdg

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ function Home() {
 
   return (
     <div className='_Home'>
-
       <section id="hero" className="hero d-flex align-items-center">
         <div className="container">
           <div className="row gy-4 d-flex justify-content-between">
@@ -64,7 +63,7 @@ function Home() {
         <div className="container" >
 
           <div data-aos="fade-up">
-            <div class="section-header">
+            <div className="section-header">
               <span>Présentation</span>
               <h2>Présentation</h2>
 
@@ -88,28 +87,28 @@ function Home() {
                 <li>
                   <img src="/assets/img/icons/icone_4.png" alt="" />
                   <div>
-                    <h5>Les influenceurs d'opinion publique</h5>
+                    <h3>Les influenceurs d'opinion publique</h3>
                     <p>Nous identifions et mobilisons la diaspora, ainsi que faisons du lobbying dans différentes villes à travers le monde. </p>
                   </div>
                 </li>
                 <li>
                   <img src="/assets/img/icons/icone_1.png" alt="" />
                   <div>
-                    <h5>Les professionnels d'un secteur économique</h5>
+                    <h3>Les professionnels d'un secteur économique</h3>
                     <p>Nous mettons en contact des professionnels et des entrepreneurs dans le business dans de nombreuses villes.</p>
                   </div>
                 </li>
                 <li >
                   <img src="/assets/img/icons/icone_0.png" alt="" />
                   <div>
-                    <h5>Les entrepreneurs et les porteurs de projet</h5>
+                    <h3>Les entrepreneurs et les porteurs de projet</h3>
                     <p>Nous offrons notre expertise pour aider les entreprises à faire le business dans la diaspora.</p>
                   </div>
                 </li>
                 <li >
                   <img src="/assets/img/icons/icone_2.png" alt="" />
                   <div>
-                    <h5>La création d'emplois dans l'économie</h5>
+                    <h3>La création d'emplois dans l'économie</h3>
                     <p>Nous développons des projets qui contribuent à la création d'emplois et à la croissance économique</p>
                   </div>
                 </li>
@@ -125,7 +124,7 @@ function Home() {
 
           <div className="row justify-content-center">
             <div className="col-lg-8 text-center">
-            <div class="section-header">
+            <div className="section-header">
               <span>Nos actions</span>
               <h2>Nos actions</h2>
 
@@ -143,72 +142,72 @@ function Home() {
 
         </div>
       </section>
-      <section id="faq" class="faq">
-        <div class="container" data-aos="fade-up">
+      <section id="faq" className="faq">
+        <div className="container" data-aos="fade-up">
 
-          <div class="section-header">
+          <div className="section-header">
             <span>Foire aux questions</span>
             <h2>Foire aux questions</h2>
 
           </div>
  <br />
-          <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="200">
-            <div class="col-lg-10">
+          <div className="row justify-content-center" data-aos="fade-up" data-aos-delay="200">
+            <div className="col-lg-10">
 
-              <div class="accordion accordion-flush" id="faqlist">
+              <div className="accordion accordion-flush" id="faqlist">
 
-                <div class="accordion-item">
-                  <h3 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
-                      <i class="bi bi-question-circle question-icon"></i>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
+                      <i className="bi bi-question-circle question-icon"></i>
                       Qui peut m’aider dans mon projet ?
                     </button>
                   </h3>
-                  <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                    <div class="accordion-body">
+                  <div id="faq-content-1" className="accordion-collapse collapse" data-bs-parent="#faqlist">
+                    <div className="accordion-body">
                       Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h3 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
-                      <i class="bi bi-question-circle question-icon"></i>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                      <i className="bi bi-question-circle question-icon"></i>
                       Comment je peux protéger mon idée ? 
                     </button>
                   </h3>
-                  <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                    <div class="accordion-body">
+                  <div id="faq-content-2" className="accordion-collapse collapse" data-bs-parent="#faqlist">
+                    <div className="accordion-body">
                       Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h3 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
-                      <i class="bi bi-question-circle question-icon"></i>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                      <i className="bi bi-question-circle question-icon"></i>
                       Est-ce que je peux être salarié et entrepreneur ? 
                     </button>
                   </h3>
-                  <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                    <div class="accordion-body">
+                  <div id="faq-content-3" className="accordion-collapse collapse" data-bs-parent="#faqlist">
+                    <div className="accordion-body">
                       Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h3 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
-                      <i class="bi bi-question-circle question-icon"></i>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
+                      <i className="bi bi-question-circle question-icon"></i>
                       Comment je trouve des professionnels ? 
                     </button>
                   </h3>
-                  <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                    <div class="accordion-body">
-                      <i class="bi bi-question-circle question-icon"></i>
+                  <div id="faq-content-4" className="accordion-collapse collapse" data-bs-parent="#faqlist">
+                    <div className="accordion-body">
+                      <i className="bi bi-question-circle question-icon"></i>
                       Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
                     </div>
                   </div>
