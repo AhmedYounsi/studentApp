ad3e3679138346273ef0de52cb829dcdfd528f29
YounsiAhmed <hammoua07@gmail.com>
2022-07-30 14:55:16 +0100

    dfg

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ function Home() {
   return (
     <div className='_Home'>
         <Head>
-        <title>APRIM Agir Pour la Révolution Industrielle et Monaitaire et Sociale</title>
+        <title>APRIM Agir Pour la Révolution Industrielle et Monaitére et Sociale</title>
       </Head>
       <section id="hero" className="hero d-flex align-items-center">
         <div className="container">
@@ -21,7 +21,7 @@ function Home() {
             <div
               data-aos="flip-left"
               className=" col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-              <h1>Agir Pour la Révolution Industrielle et Monaitaire et Sociale</h1>
+              <h1>Agir Pour la Révolution Industrielle et Monaitére et Sociale</h1>
               <p>
                 La mission de APRIMS est de mettre en relation professionnels et entrepreneurs
                 afin d{"'"}agir comme catalyseur du développement économique
@@ -145,7 +145,38 @@ function Home() {
 
         </div>
       </section>
-   <Faq /> 
+
+
+   <section id="contact" className="contact">
+   <div className="section-header">
+              <span>Envoyez-nous</span>
+              <h2>Envoyez-nous</h2>
+
+            </div>
+   <div className="form-contact row gy-4">
+            <div className="col-12">
+              <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+                <div className="row">
+                  <div className="col-md-6 form-group">
+                    <input type="text" name="name" className="form-control" id="name" placeholder="Nom et prénom" required />
+                  </div>
+                  <div className="col-md-6 form-group mt-3 mt-md-0">
+                    <input type="email" className="form-control" name="email" id="email" placeholder="Email" required />
+                  </div>
+                </div>
+                <div className="form-group mt-3">
+                  <input type="text" className="form-control" name="subject" id="subject" placeholder="Sujet" required />
+                </div>
+                <div className="form-group mt-3">
+                  <textarea className="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                </div>
+    
+                <div className="text-center"><button type="submit">Envoyer Message</button></div>
+              </form>
+            </div>
+
+          </div>
+   </section>
     </div>
   )
 }
