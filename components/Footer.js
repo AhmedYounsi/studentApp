e3f8d92850d5ea3efc5381d26a03936c74ac0f3f
YounsiAhmed <hammoua07@gmail.com>
2022-09-23 01:12:49 +0100

    dfg

--- components/Footer.js
+++ components/Footer.js
@@ -24,7 +24,7 @@ function Footer() {
               <img src="/assets/img/aprims_white_2.png" alt="" />
             </a>
             <p>
-              APRIMS est réseau d'affaires d'influence qui évolue dans différents secteurs économiques. Nous
+              APRIMS est réseau d{"'"}affaires d{"'"}influence qui évolue dans différents secteurs économiques. Nous
               travaillons selon nos objectifs et nos valeurs communes
               afin de contribuer notre expertise au développement économique
             </p>
